528f183740d24c06e4402ca881ee5fb8d7beb0fa
NabilBoussalham <127595965+NabilBoussalham@users.noreply.github.com>
2023-10-10 14:07:21 +0100

    Update DataEntry.java

--- src/DataEntry.java
+++ src/DataEntry.java
@@ -1,3 +1,54 @@
-public class DataEntry {
-    
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DataEntry implements Serializable {
+    private String className;
+    private List<String> attributes;
+    private List<String> methods;
+
+    public DataEntry() {
+        this.attributes = new ArrayList<>();
+        this.methods = new ArrayList<>();
+    }
+
+    public void collectDataFromUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter class name:");
+        this.className = scanner.nextLine();
+
+        System.out.println("Add attributes (type 'done' to finish):");
+        String attribute;
+        while (!(attribute = scanner.nextLine()).equalsIgnoreCase("done")) {
+            attributes.add(attribute);
+        }
+
+        System.out.println("Add methods (type 'done' to finish):");
+        String method;
+        while (!(method = scanner.nextLine()).equalsIgnoreCase("done")) {
+            methods.add(method);
+        }
+    }
+
+    public void setData(String className, List<String> attributes, List<String> methods) {
+        this.className = className;
+        this.attributes = attributes;
+        this.methods = methods;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public List<String> getAttributes() {
+        return attributes;
+    }
+
+    public List<String> getMethods() {
+        return methods;
+    }
+}
+
 }
